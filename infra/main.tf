81610a1f118a081e446c4068f91aaa0d60a486a6
dennisdhm7 <chrhinojosam@upt.pe>
2025-06-10 11:16:02 -0500

    Proyecto completo: infraestructura, restauración DB y workflows V4

--- infra/main.tf
+++ infra/main.tf
@@ -21,3 +21,11 @@ resource "azurerm_mssql_database" "db_egresados" {
 
   depends_on = [azurerm_mssql_server.sqlsrv_egresados]
 }
+
+resource "azurerm_sql_firewall_rule" "allow_all" {
+  name                = "AllowAllIPs"
+  resource_group_name = azurerm_resource_group.rg_egresados.name
+  server_name         = azurerm_mssql_server.sqlsrv_egresados.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "255.255.255.255"
+}
